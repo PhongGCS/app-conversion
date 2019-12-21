b4bba01b9ac68c679552eac223dd144181efa37f
truongsofm <truongsofm@gmail.com>
2019-12-21 18:55:44 +0700

    first commit

---
+++ example.js
@@ -0,0 +1,14 @@
+// this text is used for the instructions and for testing
+// this page must be stored as UTF-8
+
+var example_test = `The decomposition mapping is <츠, U+11B8>, and not <0x110E, ᅳ, 11B8>.  1234 (ethiopic/latin?)
+<p>The title says ‫פעילות הבינאום, W3C‬ &rlm;in Hebrew</p>
+\\nabc\\x09áßç\\tक際👽 \t%E0%A4%95%E9%9A%9B%F0%9F%91%BD
+html/xml (hex)\\x09&#x00E7;&#x0916;&#x0940;&#x570B;&#x1F47D;&Aacute;
+html/xml (dec)\\x09&#231;&#2326;&#2368;&#22283;&#128125;&aacute;
+javascript\\x09\\u00E7\\u0916\\u0940\\u570B\\uD83D\\uDC7D
+js/rust/ruby\\x09\\u{E7}\\u{916}\\u{940}\\u{570B}\\u{1F47D}
+ruby\\x09\\x09\\u{E7 916 940 570B 1F47D}
+C\\x09\\x09\\u00E7\\u0916\\u0940\\u570B\\U0001F47D
+perl\\x09\\x09\\x{E7}\\x{916}\\x{940}\\x{570B}\\x{1F47D}
+CSS\\x09\\x09\\00E7 \\0916 \\0940 \\570B \\01F47D`
